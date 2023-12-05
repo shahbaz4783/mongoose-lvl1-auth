9d1d0459a7730c89b9576928e5c75300ce7a6007
shahbaz4783 <shahbaz4783@gmail.com>
2023-12-05 12:00:12 +0530

    becrypt hashing

--- controllers/login.js
+++ controllers/login.js
@@ -1,11 +1,14 @@
 import md5 from 'md5';
 import User from '../models/Users.js';
+import bcrypt from 'bcrypt';
+
 
 export const loginController = (req, res) => {
 	res.render('login.ejs');
 };
 
-
+// md5 hashing
+/*
 export const postLogin = async (req, res) => {
 	try {
 		const email = req.body.email;
@@ -15,6 +18,33 @@ export const postLogin = async (req, res) => {
 
 		if (user) {
 			if (user.password === password) {
+				res.render('secret.ejs');
+			} else {
+				res.status(401).send('Your Email or Password is Incorrect');
+			}
+		} else {
+			res.status(404).send('User not found');
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Internal Server Error');
+	}
+};
+*/
+
+// becrypt
+
+export const postLogin = async (req, res) => {
+	try {
+		const email = req.body.email;
+		const password = req.body.password;
+
+		const user = await User.findOne({ email: email });
+
+		if (user) {
+			const passwordMatch = await bcrypt.compare(password, user.password);
+
+			if (passwordMatch) {
 				res.render('secret.ejs');
 			} else {
 				res.status(401).send('Your Email or Password is Incorrect');
--- controllers/register.js
+++ controllers/register.js
@@ -1,10 +1,13 @@
-import md5 from 'md5';
 import User from '../models/Users.js';
+import md5 from 'md5';
+import bcrypt from 'bcrypt';
 
 export const registerController = (req, res) => {
 	res.render('register.ejs');
 };
 
+// md 5 Encryption
+/*
 export const postRegister = async (req, res) => {
 	try {
 		const email = req.body.email;
@@ -16,6 +19,29 @@ export const postRegister = async (req, res) => {
 		});
 		await user.save();
         res.redirect('/login')
+
+	} catch (error) {
+		console.log(error);
+	}
+};
+*/
+
+// bcrypt
+export const postRegister = async (req, res) => {
+	try {
+        const saltRounds = 10;
+		const email = req.body.email;
+		const password = req.body.password;
+
+		const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+		const user = new User({
+			email: email,
+			password: hashedPassword,
+		});
+
+		await user.save();
+		res.redirect('/login');
 
 	} catch (error) {
 		console.log(error);
